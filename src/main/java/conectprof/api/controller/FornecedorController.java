916a2aefe649b97518e9499429d12c1c509614fd
caio-cesar-dev <caiocesar.souza@hotmail.com>
2026-09-23 21:13:34 -0300

    Listagem de fornecedores concluido

--- src/main/java/conectprof/api/controller/FornecedorController.java
+++ src/main/java/conectprof/api/controller/FornecedorController.java
@@ -1,16 +1,16 @@
 package conectprof.api.controller;
 
 import conectprof.api.fornecedor.DadosFornecedorDto;
+import conectprof.api.fornecedor.DadosListagemFornecedorDto;
 import conectprof.api.fornecedor.FornecedorEntity;
 import conectprof.api.fornecedor.FornecedorRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("fornecedores")
@@ -23,6 +23,11 @@ public class FornecedorController {
     @Transactional
     public void cadastrarFornecedor(@RequestBody @Valid DadosFornecedorDto dadosFornecedor){
         repository.save(new FornecedorEntity(dadosFornecedor));
+    }
+
+    @GetMapping
+    public Page<DadosListagemFornecedorDto> listarFornecedores(@PageableDefault(sort = {"nomeFantasia"}, size = 10) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemFornecedorDto::new);
     }
 
 
---
+++ src/main/java/conectprof/api/fornecedor/DadosListagemFornecedorDto.java
@@ -0,0 +1,8 @@
+package conectprof.api.fornecedor;
+
+public record DadosListagemFornecedorDto(Long id, String nomeFantasia, String email, String cnpj) {
+
+    public DadosListagemFornecedorDto(FornecedorEntity fornecedor){
+        this(fornecedor.getId(), fornecedor.getNomeFantasia(), fornecedor.getEmail(), fornecedor.getCnpj());
+    }
+}
